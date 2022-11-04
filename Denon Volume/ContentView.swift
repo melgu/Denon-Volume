14849dc466389ff659304a2bc1ff7d6234481581
Melvin Gundlach <mail@melvin-gundlach.de>
2022-11-04 13:04:43 +0100

    Add basic app structure

--- Denon Volume/ContentView.swift
+++ Denon Volume/ContentView.swift
@@ -8,19 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+	@ObservedObject var model: Model
+	
 	var body: some View {
 		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundColor(.accentColor)
-			Text("Hello, world!")
+			ZStack {
+				HStack {
+					Button {
+						model.showInformation()
+					} label: {
+						Image(systemName: "info")
+					}
+					.accessibilityLabel("Information")
+					.help("Information")
+					
+					Spacer()
+					
+					Toggle(isOn: $model.isOn) {
+						EmptyView()
+					}
+					.accessibilityLabel("Is on")
+					.help("Is on")
+				}
+				
+				Text("Network Name")
+			}
+			
+			TextField("Address", text: $model.address) {
+				model.commitAddress()
+			}
+			
+			Slider(value: $model.volume, in: 0...70, step: 1) {
+				EmptyView()
+			}
+			.accessibilityLabel("Volume")
+			.accessibilityValue("\(Int(model.volume))")
+			.disabled(!model.isOn)
+
+			ZStack {
+				HStack {
+					Text("\(Int(model.volume))")
+						.foregroundColor(model.isOn ? .primary : .secondary)
+					
+					Spacer()
+					
+					Button {
+						model.quit()
+					} label: {
+						Image(systemName: "xmark")
+					}
+					.accessibilityLabel("Quit")
+					.help("Quit")
+				}
+				
+				Text("Volume")
+					.foregroundColor(model.isOn ? .primary : .secondary)
+			}
 		}
 		.padding()
+		.frame(width: 200)
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		ContentView(model: Model())
 	}
 }
--- Denon Volume/Denon_VolumeApp.swift
+++ Denon Volume/Denon_VolumeApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct Denon_VolumeApp: App {
+	@StateObject private var model = Model()
+	
 	var body: some Scene {
-		WindowGroup {
-			ContentView()
+		MenuBarExtra {
+			ContentView(model: model)
+		} label: {
+			icon
+		}
+		.menuBarExtraStyle(.window)
+	}
+	
+	@ViewBuilder
+	private var icon: some View {
+		if model.isOn {
+			switch model.volume {
+			case 0..<30: Image(systemName: "dial.low")
+			case 30..<50: Image(systemName: "dial.medium")
+			default: Image(systemName: "dial.high").foregroundColor(.red) // FIXME: Colors don't work
+			}
+		} else {
+			Image(systemName: "dial.low").foregroundColor(.secondary) // FIXME: Colors don't work
 		}
 	}
 }
