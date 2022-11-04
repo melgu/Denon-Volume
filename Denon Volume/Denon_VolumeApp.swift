818bc9bbf5c9bb69e8a7674e0e5e8700f1a94361
Melvin Gundlach <mail@melvin-gundlach.de>
2022-11-04 12:16:55 +0100

    Initial Commit

---
+++ Denon Volume/Denon_VolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Denon_VolumeApp.swift
+//  Denon Volume
+//
+//  Created by Melvin Gundlach on 04.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Denon_VolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
